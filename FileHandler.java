36316160f16e5c8ec9c3a500671646503cd21057
Joshua Washington <jdwashington195@gmail.com>
2024-12-06 19:20:16 -0500

    Add Javadoc documentation for FileHandler methods

--- FileHandler.java
+++ FileHandler.java
@@ -16,4 +16,33 @@ public class FileHandler {
         writer.write(content);
     }
 }
+    /**
+ * Writes content to a file.
+ * @param filename the name of the file to write to
+ * @param content the content to write to the file
+ * @throws IOException if an I/O error occurs
+ */
+public static void writeToFile(String filename, String content) throws IOException {
+    // Implementation
+}
+
+/**
+ * Reads content from a file.
+ * @param filename the name of the file to read from
+ * @return the content of the file as a string
+ * @throws IOException if an I/O error occurs
+ */
+public static String readFromFile(String filename) throws IOException {
+    // Implementation
+}
+
+/**
+ * Appends content to a file.
+ * @param filename the name of the file to append to
+ * @param content the content to append to the file
+ * @throws IOException if an I/O error occurs
+ */
+public static void appendToFile(String filename, String content) throws IOException {
+    // Implementation
+}
 }
